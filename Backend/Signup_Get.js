045558c6f0db5513e50195bf6c4bc2e071db1a68
SKY TECH - RICK <ricksarkar2005@gmail.com>
2023-12-24 02:13:18 +0530

    Signup GET Done

--- Backend/Signup_Get.js
+++ Backend/Signup_Get.js
@@ -4,21 +4,20 @@ const cookieParser = require('cookie-parser');
 
 
 function Signup_Get(req, res) {
-
     const User_Id_ = req.cookies.User_Id_;
-    
-    // res.cookie("User_Id_", "THIS IS GSB COOKIE");
     if (User_Id_ == undefined || User_Id_ == null || User_Id_ == "") {
-        res.status(200).render("Signup");
-
         
+        res.cookie("New_User", "Yes",{
+            httpOnly: true,
+            path: "/signup",
+            expires: new Date(Date.now() + 86400000),
+            secure: false
+        });
+        res.status(200).render("Signup");
     }
     else{
-        res.status(200).redirect("/")
+        res.status(302).redirect("/");
     }
-    console.log(User_Id_);
-
-
 }
 
 
