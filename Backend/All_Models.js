add2cb387fecf053edc718eba917531d6eb7a0ad
SKY TECH - RICK <ricksarkar2005@gmail.com>
2024-07-20 15:54:34 +0530

    m

--- Backend/All_Models.js
+++ Backend/All_Models.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect('mongodb://127.0.0.1:27017/User_Data');
+mongoose.connect('mongodb+srv://skytechrick:1234Rick%40%23*@gsbdb.tyzbaqw.mongodb.net/User_Data');
 
 const db = mongoose.connection;
 db.on('error',(error) => {
